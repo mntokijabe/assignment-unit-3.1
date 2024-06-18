7ef9bffe6bd37c245d18f0e1fa5e9e451eafd8b5
Elwood Nelson <elwoodnelson@gmail.com>
2024-06-18 10:00:20 -0500

    finished first 3 exercises

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -10,10 +10,12 @@ console.log('Animals are: ', animalArray);
 // 1. TODO: Make an array `favoriteFoods` with some favorite foods.
 //    Be sure to console.log your array of foods to the console with
 //    a message, similar to the example above.
-
+let favoriteFoods = ['pizza', 'samosas', 'banana cream pie', 'Sr Store donuts'];
+console.log('Favorite foods are: ', favoriteFoods);
 
 // Array.length: A property that tells you how many items are in a given array.
 console.log('--- 2. Length of an array ---');
+
 
 // Example: How many animals are in the array?
 let numberOfAnimals = animalArray.length // 👈 Using the .length property!
@@ -22,7 +24,8 @@ console.log('Number of animals:', numberOfAnimals);
 // 2. TODO: Create a variable `numberOfFoods` and use the .length property
 //    to assign it the number value of how many items are inside `favoriteFoods`.
 //    Don't forget to console.log `numberOfFoods` to make sure your code worked!
-
+let numberOfFoods = favoriteFoods.length;
+console.log('Number of favorite foods:', numberOfFoods);  //displaying the number of foods
 
 // Accessing array items
 console.log('--- 3. Accessing items in an array ---');
@@ -35,16 +38,24 @@ console.log('First animal is', firstAnimal);
 //      of the second item from the `animalArray`.
 //      Be sure to console.log this new variable to make sure your code
 //      worked as expected.
-
+let secondAnimal = animalArray[1];
+console.log('The second animal is',secondAnimal);  //shows the second animal
 
 // 3.b. TODO: Create a variable `lastAnimal` and assign it the value of
 //      the "last" item in `animalArray`, using its array index.
 //      You'll need to console.log `animalArray` and `lastAnimal` to make
 //      sure that your code does what you want. (Never trust your code until
 //      you have proof that it works!)
+let lastAnimal = animalArray[3];
+console.log('The animals are:',animalArray);
+console.log('The last animal is',lastAnimal);
+
 
 // 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length 
 //       property, rather than the exact (hardcoded) index number of the last item.
+lastAnimal = animalArray[animalArray.length - 1];
+console.log('The animals are:',animalArray);
+console.log('The last animal is',lastAnimal);
 
 
 
