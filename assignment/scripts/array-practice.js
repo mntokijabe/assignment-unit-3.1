164d82599ee78290f2d65d50be41a1af638a592c
Elwood Nelson <elwoodnelson@gmail.com>
2024-06-18 14:56:35 -0500

    finished the stretch activities

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -129,10 +129,12 @@ let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
 
 // 5.a (STRETCH) TODO: Replace the second gem with 'Tourmaline'.
 //      Remember to console.log the updated array.
-
-
+gems[1] = 'Tourmaline';
+console.log(gems);
 // 5.b (STRETCH) TODO: Sort your gems array in reverse alphabetical order.
 //     Please console.log the sorted array.
+gems.sort().reverse();
+console.log(gems);
 
 
 // 5.c (STRETCH) TODO: Create a variable `gemString` and assign it a string value
@@ -142,6 +144,9 @@ let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
 //       "Emerald and Diamond and Tourmaline and Topaz"
 //     🔥 You'll need to research the .join Array method. 🔥
 //     Don't forget to console.log `gemString`...
+let gemString = gems.join(` and `);
+console.log(gemString);
+
 
 
 // 5.d (STRETCH) TODO: Make a new array `colorfulGems` that combines 
@@ -149,7 +154,8 @@ let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
 //     It should look something like:
 //       ['Tourmaline', 'Topaz', 'Tanzanite', 'Iolite', 'Emerald', 'Diamond', 'yellow', 'green']
 //     Now, console.log the new array.
-
+let colorfulGems = gems.concat(colors);
+console.log(colorfulGems);
 
 
 
